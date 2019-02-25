2e256c73c6961de91e69111af98c073730ec68b2
Sakari <Sakari@192.168.1.5>
2019-02-25 13:16:14 +0200

    commit2

--- src/Laskuri/Laskuri.java
+++ src/Laskuri/Laskuri.java
@@ -22,10 +22,13 @@ public class Laskuri{
 	public Laskuri(){
 		this.tapahtumia = 0;
 	}
-
+	
   	public void lisaa(){
     		this.tapahtumia = this.tapahtumia + 1; 
   	}
+        public void lisaa(int lisays){
+            this.tapahtumia = this.tapahtumia + lisays;
+        }
   	public int lukema(){
     		return this.tapahtumia;
   	}
@@ -33,7 +36,7 @@ public class Laskuri{
     		this.tapahtumia = 0;
   	}
 	public String toString(){
-		return "Laskurin lukema on " + this.tapahtumia;
+		return "Lukema " + this.tapahtumia;
 	}
 
 }
